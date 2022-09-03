9489cdf3f7123962aed33c7173305059d94d115e
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-03 19:00:38 +0200

    installed some dependencies and styled the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// Importing the styling 
+import '../css/App.css';
+
 // Creating the App component
 const App = () => {
     return(
-        <div>App</div>
+        <div className="container" style={{backgroundImage: "url(./ques1.png"}}>
+        </div>
     )
 }
 
