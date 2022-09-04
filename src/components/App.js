4e384a9c0a365542aa73c0a651067585bd55ec92
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-04 12:34:35 +0200

    Fetching questions and answers for the quiz

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 // Importing the costume made components
 import Header from './Header';
@@ -15,9 +16,13 @@ import '../css/App.css';
 const App = () => {
 
     const [name, setName] = useState("");
+    const [questions, setQuestions] = useState();
+    const [score, setScore] = useState(0);
 
-    const fetchQuestions = () => {
-
+    const fetchQuestions = async(category, difficulty) => {
+        const { data } = await axios.get(`https://opentdb.com/api.php?amount=15&category=${category}&difficulty=${difficulty}&type=multiple`);
+        console.log(data);
+        setQuestions(data.results)
     }
 
     return(
@@ -26,7 +31,7 @@ const App = () => {
                 <Header title="Quiz Game" />
                 <Routes>
                     <Route path="/" exact element={<HomePage name={name} setName={setName} fetchQuestions={fetchQuestions} />} />
-                    <Route path="/quiz" exact element={<QuizPage />} />
+                    <Route path="/quiz" exact element={<QuizPage name={name} questions={questions} score={score} setScore={setScore} setQuestions={setQuestions} />} />
                     <Route path="/result" exact element={<ResultPage />} />
                 </Routes>
             </div>
--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Creating the QuizPage component
-const QuizPage = () => {
+const QuizPage = ({ name, score, questions, setQuestions, setScore}) => {
+
+    const [options, setOptions] = useState();
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+
+    useEffect(() => {
+        console.log(questions);
+
+        setOptions(questions && handleSchuffle([questions[currentQuestion]?.correct_answer, ...questions[currentQuestion]?.incorrect_answers ]));
+        console.log(options)
+    }, [questions])
+
+    const handleSchuffle = (opt) => {
+        return opt.sort(() => Math.random() - 0.5)
+    }
+
     return (
         <div>QuizPage</div>
     )
