b8ca303d1132e4699d3f22e2ca528871ccf2ecf5
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-01-29 21:04:26 +0100

    Reconstructing the quiz game app

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,9 @@
-import React, { useState } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
-// Importing the costume made components
-import Header from './Header';
-import Footer from './Footer';
-import HomePage from './HomePage';
-import QuizPage from './QuizPage';
-import ResultPage from './ResultPage';
-
-// Importing the styling 
-import '../css/App.css';
+import React from "react";
 
 // Creating the App component
 const App = () => {
+  return <div className="app-container">App</div>;
+};
 
-    const [name, setName] = useState("");
-    const [questions, setQuestions] = useState();
-    const [score, setScore] = useState(0);
-
-    return(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <div className="container" style={{backgroundImage: "url(./ques1.png"}}> 
-                <Header title="Quiz Game" />
-                <Routes>
-                    <Route path="/" exact element={<HomePage name={name} setName={setName} setQuestions={setQuestions} />} />
-                    <Route path="/quiz" exact element={<QuizPage name={name} questions={questions} score={score} setScore={setScore} setQuestions={setQuestions} />} />
-                    <Route path="/result" exact element={<ResultPage score={score} name={name} />} />
-                </Routes>
-            </div>
-            <Footer />
-        </BrowserRouter>
-    )
-}
-
+// Exporting the App component
 export default App;
