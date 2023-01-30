86430e4c6455335aad2c18fe408f16ee629bd514
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-01-30 20:26:51 +0100

    Added new Navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Importing the costume made components
+import Navigation from "./Navigation";
 
 // Importing the style file
 import "../css/App.css";
 
 // Creating the App component
 const App = () => {
-  return <div className="app-container">App</div>;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="app-container">
+        <Navigation />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 // Exporting the App component
---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Importing the style file
+import "../css/Navigation.css";
+
+// Creating the Navigation component
+const Navigation = () => {
+  return (
+    <header>
+      <Link to="/">
+        <h1>Quiz me</h1>
+      </Link>
+      <small>Let's see how much you know</small>
+    </header>
+  );
+};
+
+// Exporting the Navigation component
+export default Navigation;
