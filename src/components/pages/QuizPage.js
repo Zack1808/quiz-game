c45dd750ecfaede5892868be14b952512f0efd63
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-02-03 12:23:38 +0100

    Fixed bug

--- src/components/pages/QuizPage.js
+++ src/components/pages/QuizPage.js
@@ -22,6 +22,7 @@ const QuizPage = ({ questions, player }) => {
     setStep((prevState) => prevState + 1);
   };
 
+  if (!questions) return null;
   return (
     <div className="quiz-container">
       <header>
